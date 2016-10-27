48ad0f81e7708bf0bad3c553a1809191e855c62e
Peter Adam <peter.adam02@gmail.com>
2016-10-27 23:48:11 +0100

    Create setup and test file structures

---
+++ code/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Numerical Analysis and Software Programming Assignment 2',
+    'author': 'Peter Adam, Kieron Ellis, Andy McSweeney',
+    'url': 'https://github.com/Padam-0/nas_prog2',
+    'download_url': 'https://github.com/Padam-0/nas_prog2',
+    'author_email': 'peter.adam@ucdconnect.ie, kieron.ellis@ucdconnect.ie, andy.mcsweeney@ucdconnect.ie',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['bsm', 'sor'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
---
+++ code/sor.py
@@ -0,0 +1,5 @@
+"""
+Write a program to implement the Spare-SOR algorithm for solving a system of n linear equations in n unknowns.
+Ax = b
+with A a nxn matrix in R and x, b nx1 vectors in R
+"""
