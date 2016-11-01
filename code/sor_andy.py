4fd6e00b6cf98561783dafc816fceafc61d76cd3
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-01 21:58:59 +0000

    Extra Sor.in test file added

---
+++ code/sor_andy.py
@@ -0,0 +1,80 @@
+"""
+Write a program to implement the Spare-SOR algorithm for solving a system of n
+linear equations in n unknowns.
+Ax = b
+with A a nxn matrix in R and x, b nx1 vectors in R
+"""
+
+import numpy as np
+
+def read_inputs(filename):
+
+    matrix_size = np.genfromtxt(filename, max_rows=1)
+    print('Matrix size n:',matrix_size)
+    print('----------')
+
+    matrix_in = np.genfromtxt(filename, skip_header=1, skip_footer=1)
+    print('Matrix A:')
+    print(matrix_in)
+    print('----------')
+
+    column = np.genfromtxt(filename, usecols=0)
+    col_len = len(column)
+    print('Column length', col_len)
+    print('----------')
+
+    vector_b = np.genfromtxt(filename, skip_header=(col_len-1))
+    print('Vector b: ',vector_b)
+
+    return matrix_size, matrix_in, vector_b
+
+
+def write_outputs(solution_vector, stopping_reason, other_information):
+    pass
+
+
+def calc_vector_norms(vector):
+    pass
+
+
+def zero_diag(matrix):
+    # Check if there are 0's on the diagonal of the input matrix
+
+    if some_condition:
+        return True
+    else:
+        return False
+
+def s_diag_dominant(matrix):
+    # Check if the diagonal value is larger than the sum of all
+    # other entries in that row/column
+
+    if some_condition:
+        return True
+    else:
+        return False
+
+def divergence(matrix):
+    pass
+    # Check for divergence in successive matrix norms
+
+    # I think this actually needs to go inside solve_matrix()
+    # but I'll leave it here for now
+
+
+def solve_matrix(A):
+    pass
+    # Check for cycling
+
+    # Define maxits?
+
+    # Stop when maxits reached
+
+
+def main():
+    matrix_size, matrix_in, vector_b = read_inputs('nas_Sor2.in')
+
+
+
+if __name__=='__main__':
+    main()
