bfedd9480b3fc02f5f05e4bd2b086952078dc460
Peter Adam <peter.adam02@gmail.com>
2016-11-02 21:19:27 +0000

    Fix testing for BSM. Fix rowStart for BSM

--- code/tests/sor_andy_tests.py
+++ code/tests/sor_andy_tests.py
@@ -18,11 +18,12 @@ def test_create_BS_matrix():
     resM0 = bsm.create_BS_matrix(M0, T/M0, r, theta)
     resM1 = bsm.create_BS_matrix(M1, T/M1, r, theta)
 
-    #assert_equal(resMn, "There must be at least 3 intervals")
-    #assert_equal(resM0, "There must be at least 3 intervals")
-    #numpy.testing.assert_array_equal(resM1[0], np.array([]))
-    #numpy.testing.assert_array_equal(resM1[1], np.array([0,1,0,1,2,1,2]))
-    #numpy.testing.assert_array_equal(resM1[2], np.array([0,2,5,8]))
+    assert_equal(resMn[0], "There must be at least 3 intervals")
+    assert_equal(resM0[0], "There must be at least 3 intervals")
+    numpy.testing.assert_array_almost_equal(resM1[0], np.array([2.1, -0.55,
+        -1.6, 4.8, -2, -3.75, 9.3]), decimal=6)
+    numpy.testing.assert_array_equal(resM1[1], np.array([0,1,0,1,2,1,2]))
+    numpy.testing.assert_array_equal(resM1[2], np.array([0,2,5,7]))
 
 
 def test_con_to_csr():
