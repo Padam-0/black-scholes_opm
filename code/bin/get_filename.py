34fc98709b2d29e50e1cab6aaa8a57797b6b1e4a
Peter Adam <peter.adam02@gmail.com>
2016-11-05 18:25:30 +0000

    Finish writing docstring and comments for get_filename.py

--- code/bin/get_filename.py
+++ code/bin/get_filename.py
@@ -100,48 +100,78 @@ def check_CM_args(cmArgs):
                 else:
                     # If user elects to exit
                     if input('Would you like to exit? [y/n]: ').upper() == 'Y':
+                        # Exit
                         exit()
-                    # If not, restart while loop
+                    # If not:
                     else:
+                        # Restart loop
                         continue
+            # If user answers neither Y or N
             else:
-                # If user answers neither Y or N
+                # If the user wants to try again
                 if input('Looks like an error. Would you like to try '
                                     'again? [y/n]: ').upper() =='Y':
+                    # Restart loop
                     continue
+                # If the user wants to exit
                 else:
+                    # Exit
                     exit(0)
-
+        # If 2 command line arguments are given, 'sor.py' and the input file
+        # name
         elif len(cmArgs) == 2:
+            # Append the input file name to the file names list
             file_names.append(cmArgs[1])
+
+            # Prompt the user for an output file name
             question = input('Would you like to define an output '
-                             'filename? [y/n]: ').upper()
+                             'file name? [y/n]: ').upper()
+            # If the user wants to enter one manually
             if question == 'Y':
                 file_names.append(input("Please enter an output file name: "))
+                # If the user doesn't want to enter a file name:
             elif question == 'N':
+                # Asks the user if they want to use the default file name
                 if input('Is using the default file name ok? [y/n]: '
                                      '').upper() =='Y':
+                    # Append default file name to the file name list
                     file_names.append('nas_Sor.out')
+                # If not, ask to exit
                 else:
+                    # If user elects to exit
                     if input('Would you like to exit? [y/n]: ').upper() == 'Y':
-                        exit("Default Exit Message")
+                        exit()
+                    # Otherwise, restart loop
                     else:
+                        # Restart loop
                         continue
+        # If three commands are given, 'sor.py', input file name, and output
+        # file name
         elif len(cmArgs) == 3:
+            # Append the given file names to the file names list
             file_names.append(cmArgs[1])
             file_names.append(cmArgs[2])
+        # In all other circumstances, use default file names
         else:
             file_names = ['nas_Sor.in', 'nas_Sor.out']
 
+        # Check given file names are real files that can be opened
         if not check_file_exists(con_filename(file_names[0], 1)[0]):
+            # If not, prompt the user to try again
             if input("I can't find that file. Would you like to try "
                 "again? [y/n]: ").upper() == 'Y':
+                # Restart loop
                 continue
+            # If the user does not want to try agiain
             else:
+                # Exit
                 exit(0)
 
+    # Convert the input file name to required format
     input_file, sample_file = con_filename(file_names[0], 1)
+    # Convert the output file name to required format
     output_file = con_filename(file_names[1], 2, sample_file)[0]
+    # Return file names
     return input_file, output_file
 
 
@@ -168,4 +198,5 @@ def con_filename(filename, argNum = 0, sample_file = False):
         elif argNum == 2:
             filename = os.path.join('sample_outputs', filename)
 
+    # Return converted file name and if it is a sample file
     return filename, sample_file
