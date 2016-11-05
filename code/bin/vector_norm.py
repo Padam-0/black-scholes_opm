89eee9cb4d4ee84ed0e7a8f8769f26b9bb885ba2
Peter Adam <peter.adam02@gmail.com>
2016-11-05 22:14:54 +0000

    Write comments and docstring for vector_norm.py

--- code/bin/vector_norm.py
+++ code/bin/vector_norm.py
@@ -1,9 +1,34 @@
 import math
 
-#||v|| = sqrt(v1^2 + v2^2 +... +vn^2)
+"""
+raw_input_check.py
 
-def vectornorm(v):
-    norm = 0
-    for element in v:
-        norm += element ** 2
-    return math.sqrt(norm)
+This module contains 1 function, vectornorm(), which takes 1 argument.
+This is:
+
+v - A 1 Dimensional Numpy array containing n elements, all floats.
+
+The purpose of vectornorm() is to calculate the euclidean norm of a given
+vector. In this context, the norm of the vector v (||v||) is defined as:
+
+||v|| = sqrt(v1^2 + v2^2 +... +vn^2)
+
+Or the square root of the sum of the squares of all the individual entries
+of the vector.
+
+vectornorm() returns a float value that is the norm of the vector v.
+
+"""
+
+def vectornorm(vector):
+    # Calculate the norm of the input vector
+
+    # Initialize norm to 0
+    sum_of_squares = 0
+    # For each element in the vector
+    for element in vector:
+        # Increase the sum of squares by the square of the given element
+        sum_of_squares += element ** 2
+
+    # Return the square root of the sum of squares, or the norm of the vector
+    return math.sqrt(sum_of_squares)
