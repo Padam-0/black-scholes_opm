fefbb4b2a1c0b5997bf8792a8b64584a60e3c3c4
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-05 17:57:48 +0000

    Working on test_calculate_residual

--- code/tests/sor_tests.py
+++ code/tests/sor_tests.py
@@ -8,6 +8,44 @@ from code import bsm
 from code.bin import get_filename, raw_input_check, read_inputs, input_tests, \
     convert_to_csr, value_tests
 
+# ------------------------
+# calculate_residual tests
+# ------------------------
+
+"""
+This function contains values to test the residual function with calculate_residual.py
+"""
+
+def test_calculate_residual():
+    val = [13.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 14.0]
+    col = np.array([0,  3,  0,  1,  3,  0,  1,  2,  3,  0,  2,  3])
+    rowStart = np.array(0, 2, 5, 9, 12)
+    b = np.array([1, 2, 3, 4] )
+    x = np.array([-0.00979992, 0.08289098, 0.06390363, 0.28184973])
+
+    check = 2.5404391594785375e-10
+
+    res1 = calculate_residual.residual(val, col, rowStart, b, x)
+
+    numpy.testing.assert_almost_equal(res1, check)
+
+
+    """
+    Test required to find residual
+    residual is what is returned
+
+    Residual for current nas_Sor.in is 1.5337009273677185e-10
+
+    Contents of nas_Sor.in
+    4
+    13 0 0 4
+    4 11 0 4
+    7 8 20 4
+    1 0 1 14
+    1 2 3 4
+
+    """
+
 def test_check_CM_args():
     res1 = get_filename.check_CM_args(["sor_andy.py", "nas_Sor.in",
                                      "nas_out.out"])
