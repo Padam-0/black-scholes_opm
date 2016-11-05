5cc909c6eabc603dbc4560bcc4dbcb99f8556af9
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-05 18:39:51 +0000

    update

--- code/bin/calculate_residual.py
+++ code/bin/calculate_residual.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+
 import numpy as np
 from bin import vector_norm
 
--- code/tests/sor_tests.py
+++ code/tests/sor_tests.py
@@ -40,6 +40,22 @@ def test_calculate_residual():
 
     """
 
+# --------------------
+# convert_to_csr tests
+# --------------------
+
+def test_con_to_csr():
+    res1 = convert_to_csr.con_to_csr(np.array(
+            [[12, 0, 0], [4, 11, 0],[7, 8, 16]]),
+                                3.0)
+
+    numpy.testing.assert_array_equal(res1[0],
+                    np.array([12,4,11,7,8,16]))
+    numpy.testing.assert_array_equal(res1[1],
+                    np.array([0,0,1,0,1,2]))
+    numpy.testing.assert_array_equal(res1[2], np.array([0,1,3,6]))
+
+
 def test_check_CM_args():
     res1 = get_filename.check_CM_args(["sor_andy.py", "nas_Sor.in",
                                      "nas_out.out"])
@@ -116,16 +132,7 @@ def test_create_BS_matrix():
     numpy.testing.assert_array_equal(resM1[2], np.array([0,2,5,7]))
 
 
-def test_con_to_csr():
-    res1 = convert_to_csr.con_to_csr(np.array(
-            [[12, 0, 0], [4, 11, 0],[7, 8, 16]]),
-                                3.0)
 
-    numpy.testing.assert_array_equal(res1[0],
-                    np.array([12,4,11,7,8,16]))
-    numpy.testing.assert_array_equal(res1[1],
-                    np.array([0,0,1,0,1,2]))
-    numpy.testing.assert_array_equal(res1[2], np.array([0,1,3,6]))
 
 
 def test_zero_diag():
