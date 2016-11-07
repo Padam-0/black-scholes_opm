bf2f8b0e63ea3f7636194c5bb451f42b16b361e8
Kieron Ellis <Kieron Ellis>
2016-11-07 18:32:37 +0000

    added a b vector to matrix market function

--- code/matrix_market_to_CSR.py
+++ code/matrix_market_to_CSR.py
@@ -1,11 +1,13 @@
 import scipy.io
+import numpy as np
 def mm_to_CSR(file1):
     A = scipy.io.mmread(file1)
+    size = A.shape[0]
+    for i in range(size):
+        b = np.random.randint(-100, 100, size=(n))
     A = A.tocsr()
     value = A.data
     column_index = A.indices
     row_pointers = A.indptr
-    return value, column_index, row_pointers
-
-val1, col1, rowstart1 = mm_to_CSR("s3dkt3m2.mtx")
-print(val1[0])
+    return value, column_index, row_pointers, b
+val1, col1, row1, b = mm_to_CSR("s3dkt3m2.mtx")
