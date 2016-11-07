82363250a3db88231de07cb0aac7b402f350b658
Peter Adam <peter.adam02@gmail.com>
2016-11-07 17:40:39 +0000

    Fix errors in printing output file

--- code/bin/write_output.py
+++ code/bin/write_output.py
@@ -7,24 +7,23 @@ write_output.py
 
 def output_text_file(output_file_name, stopping_reason, maxit, iterations,
                      mach_e, x_seq_tol, res_tol,w, x):
-    f = open(output_file_name, 'w')
-    # Output solution_vector_x, stopping_reason, maxits, #_of_iterations,
-    # machine_epsilon, x-seq_tolerance, residual, w
-    first_line = ["Stopping Reason", "Maximum Number of Iterations",
-                  "Number of Iterations", "Machine Epsilon",
-                  "X Sequence Tolerance", "Residual Sequence Tolerance"]
 
-    second_line = [stopping_reason, maxit, iterations, mach_e, x_seq_tol,
-                   res_tol]
+    with open(output_file_name, 'w') as f:
+        # Output solution_vector_x, stopping_reason, maxits, #_of_iterations,
+        # machine_epsilon, x-seq_tolerance, residual, w
+        first_line = ["Stopping Reason", "Maximum Number of Iterations",
+                      "Number of Iterations", "Machine Epsilon",
+                      "X Sequence Tolerance", "Residual Sequence Tolerance"]
 
-    spacing = 30
+        second_line = [stopping_reason, maxit, iterations, mach_e, x_seq_tol,
+                       res_tol]
 
-    f.write('| '.join(["%s" % (heading.ljust(spacing)) for heading in
-                       first_line]))
-    f.write("\n")
-    f.write('| '.join(["%s" % (str(value).ljust(spacing)) for value in
-                       second_line]))
-    f.write("\n")
-    f.write(str(x))
-    f.close()
+        spacing = 30
 
+        f.write('| '.join(["%s" % (heading.ljust(spacing)) for heading in
+                           first_line]))
+        f.write("\n")
+        f.write('| '.join(["%s" % (str(value).ljust(spacing)) for value in
+                           second_line]))
+        f.write("\n")
+        f.write(', '.join(["%s" % (str(value)) for value in x]))
