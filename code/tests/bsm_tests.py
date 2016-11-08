9a192764bf192d31d956fd14671a38418302534f
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-08 22:41:33 +0000

    create_BS_matrix.py - four tests cases written. Complete.

--- code/tests/bsm_tests.py
+++ code/tests/bsm_tests.py
@@ -3,36 +3,46 @@ import numpy.testing
 import numpy as np
 from bsm_modules import create_BS_b, create_BS_matrix
 
+"""
+
+bsm_tests.py
+
+The test module has four test cases
+
+- Test 1 tests an invalid argument, M = 2. A SystemExit is the expected return
+- Test 2 tests another invalid argument M = -2. A SystemExit is the expected return
+- Test 3 tests four valid inputs
+- Test 4 tests another invalid argument, k = "string". A TypeError is the expected return
+
+"""
+
+
 def test_create_BS_matrix():
-    Mn = -3
-    M0 = 2
-    M1 = 3
 
-    T = 30
-    r = 0.02
-    theta = 0.3
-
-
-    M = 2
+    M1 = 2
+    M2 = -2
+    M3 = 5
     X = 12
     S_Max = 20
-    k = 20
+    k1 = 5
+    k2 = "string"
     r = 0.02
     theta = 0.3
 
 
-    # resMn = create_BS_matrix.create_BS_matrix(Mn, T/Mn, r, theta)
-    # resM0 = create_BS_matrix.create_BS_matrix(M0, T/M0, r, theta)
-    # resM1 = create_BS_matrix.create_BS_matrix(M1, T/M1, r, theta)
+    res1 = create_BS_matrix.create_BS_matrix(M3, k1, r, theta)
 
-    #res1 = create_BS_matrix.create_BS_matrix(M, k, r, theta)
+    # test case 1
+    assert_raises(SystemExit, create_BS_matrix.create_BS_matrix, M1 ,k1 , r, theta)
 
-    assert_raises(SystemExit, create_BS_matrix.create_BS_matrix, M ,k, r, theta)
+    # test case 2
+    assert_raises(SystemExit, create_BS_matrix.create_BS_matrix, M2, k1, r, theta)
 
-    # assert_equal(resMn[0], "There must be at least 3 intervals")
-    # assert_equal(resM0[0], "There must be at least 3 intervals")
-    #
-    # numpy.testing.assert_array_almost_equal(resM1[0], np.array([2.1, -0.55,
-    #     -1.6, 4.8, -2, -3.75, 9.3]), decimal=6)
-    # numpy.testing.assert_array_equal(resM1[1], np.array([0,1,0,1,2,1,2]))
-    # numpy.testing.assert_array_equal(resM1[2], np.array([0,2,5,7]))
+    # test case 3
+    numpy.testing.assert_array_almost_equal(res1[0], np.array([1.55, -0.275, -0.8, 2.9, -1., -1.875, 5.15, -2.175,
+                                                               -3.4, 8.3, -3.8, -5.375, 12.35 ]), decimal=6)
+    numpy.testing.assert_array_equal(res1[1], np.array([0, 1, 0, 1, 2, 1, 2, 3, 2, 3, 4, 3, 4]))
+    numpy.testing.assert_array_equal(res1[2], np.array([0, 2, 5, 8, 11, 13]))
+
+    # test case 4
+    assert_raises(TypeError, create_BS_matrix.create_BS_matrix, M3, k2, r, theta)
