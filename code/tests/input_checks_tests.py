b43855a87182f70011e8a0e8c4528ab1c4dbe5db
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-10 18:02:46 +0000

    Updated input_check_tests

--- code/tests/input_checks_tests.py
+++ code/tests/input_checks_tests.py
@@ -18,12 +18,44 @@ def test_csr_input_checks():
     assert_equal(res1, None)
     assert_equal(res2, "Value and column vectors are not the same length")
 """
+# Matrix Values
+val1 = np.array([13.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 14.0])
+val2 = np.array([13.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 4.0, 1.0, 1.0, 14.0])
+val3 = np.array([3.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 14.0, 15.0])
 
-def test_dense_input_test():
-    matrix_size = None
-    matrix_in = None
-    vector_b = None
+col1 = np.array([0, 3, 0, 1, 3, 0, 1, 2, 3, 0, 2, 3])
+col2 = np.array([0, 3, 0, 1, 3, 0, 1, 3, 0, 2, 3, 4])
+col3 = np.array([0, 3.5, 0, 1, 3, 0, 1, 2, 3, 0, 2, 3])
 
-    #res1 = input_checks.dense_input_checks(matrix_size, matrix_in, vector_b)
+rowStart1 = np.array([0, 2, 5, 9, 12])
+rowStart2 = np.array([0, 2, 5, 8, 10])
+rowStart3 = np.array([1, 2, 5, 9, 12])
+rowStart4 = np.array([0, 2.5, 5, 9, 12])
+rowStart5 = np.array([0, 2, 5, 9, 13])
 
-    assert_equal(res1, None)
+b1 = np.array([2, 3, 4, 5])
+b2 = np.array([2, 3, 4, 5, 6])
+b3 = np.array([2, 3, 4, 5])
+b4 = np.array([2, 3, 4, 5])
+
+def test_csr_input_checks():
+    res1 = input_checks.csr_input_checks(val1, col1, rowStart1, b1)
+    res2 = input_checks.csr_input_checks(val1, col1, rowStart1, b2)
+    res3 = input_checks.csr_input_checks(val1, col2, rowStart1, b1)
+    res4 = input_checks.csr_input_checks(val1, col1, rowStart2, b1)
+    res5 = input_checks.csr_input_checks(val1, col1, rowStart3, b1)
+    res6 = input_checks.csr_input_checks(val1, col1, rowStart4, b1)
+    res7 = input_checks.csr_input_checks(val3, col1, rowStart5, b1)
+    res8 = input_checks.csr_input_checks(val1, col3, rowStart1, b1)
+
+    assert_equal(res1, [])
+    assert_equal(res2, ["Number of columns in matrix is not the same as the "
+                      "number of rows in Vector b"])
+    assert_equal(res3, ["Uneven number of rows and columns"])
+    assert_equal(res4, ["Last entry of RowStart vector is equivalent to the "
+                      "nth entry of val + 1"])
+    assert_equal(res5, ["First entry of RowStart vector is not 0"])
+    assert_equal(res6, ["RowStart vector contains non-integer entries"])
+    assert_equal(res7, ["Value and column vectors do not have the same number "
+                      "of entries"])
+    assert_equal(res8, ["Column vector contains non-integer entries"])
--- code/tests/value_checks_tests.py
+++ code/tests/value_checks_tests.py
@@ -5,19 +5,21 @@ from sor_modules import value_checks
 
 """
 value_check.py
+
 """
 # Matrix Values
 val1 = [13.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 14.0]
-val2 = [0.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 0.0]
+val2 = [13.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 4.0, 1.0, 1.0, 14.0]
 val3 = [3.0, 4.0, 4.0, 11.0, 4.0, 7.0, 8.0, 20.0, 4.0, 1.0, 1.0, 14.0]
 
 col1 = np.array([0, 3, 0, 1, 3, 0, 1, 2, 3, 0, 2, 3])
-col2 = np.array([0, 3, 0, 1, 3, 0, 1, 2, 3, 0, 2])
+col2 = np.array([0, 3, 0, 1, 3, 0, 1, 3, 0, 2, 3])
 col3 = np.array([0, 3, 0, 1, 3, 0, 1, 2, 3, 0, 2, 3])
 
 rowStart1 = np.array([0, 2, 5, 9, 12])
-rowStart2 = np.array([0, 2, 5, 9, 12])
+rowStart2 = np.array([0, 2, 5, 8, 11])
 rowStart3 = np.array([0, 2, 5, 9, 12])
+
 
 
 def test_zero_diag():
@@ -29,28 +31,17 @@ def test_zero_diag():
     assert_equal(res2, False)
     assert_equal(res3, True)
 
+
 def test_diag_dominant():
-    res1 = convert_to_csr.con_to_csr(np.array(
-            [[12, 0, 0], [4, 11, 0],
-             [7, 8, 16]]),
-                               3.0)
-    res2 = convert_to_csr.con_to_csr(np.array(
-            [[13, 0, 3, 5], [4, 11, 0, 4],
-             [7, 8, 19, 1], [1, 1, 1, 11]]),
-                               4.0)
-    res3 = convert_to_csr.con_to_csr(np.array(
-            [[12, 0, 0, 5, 7], [4, 11, 0, 4, 12],
-             [7, 8, 1, 12, 14], [1, 1, 1, 1, 1],
-             [2, 2, 2, 2, 2]]),
-                               5.0)
-    assert_equal(value_checks.diag_dominant(res1[0], res1[1], res1[2]), True)
-    assert_equal(value_checks.diag_dominant(res2[0], res2[1], res2[2]), True)
-    assert_equal(value_checks.diag_dominant(res3[0], res3[1], res3[2]), False)
+    res1 = value_checks.diag_dominant(val1, col1, rowStart1)
+    res2 = value_checks.diag_dominant(val3, col3, rowStart3)
+
+    assert_equal(res1, True)
+    assert_equal(res2, False)
 
 
 def test_value_tests():
-
-    errors = 0
+    errors = []
 
     res1 = value_checks.value_tests(val1, col1, rowStart1, errors)
     res2 = value_checks.value_tests(val2, col2, rowStart2, errors)
