65582e301f4beb0d5cd3cb317400f08f1baaa710
Peter Adam <peter.adam02@gmail.com>
2016-11-11 18:17:45 +0000

    Added additional test to import_mtx_tests

--- code/tests/import_mtx_tests.py
+++ code/tests/import_mtx_tests.py
@@ -5,8 +5,9 @@ from sor_modules import import_mtx
 
 def test_import_mtx():
     a = 'sample_inputs/sample_mtx.mtx'
+    b = 'sample_inputs/s3dkt3m2.mtx'
     res1 = import_mtx.import_mtx(a)
-    print(res1)
+    res2 = import_mtx.import_mtx(b)
 
     val = np.array([1,2,3,4,5,6,7,8,9])
     col = np.array([0,1,2,0,1,2,0,1,2])
@@ -15,6 +16,12 @@ def test_import_mtx():
     numpy.testing.assert_equal(res1[0], val)
     numpy.testing.assert_equal(res1[1], col)
     numpy.testing.assert_equal(res1[2], rowStart)
+
+
+    numpy.testing.assert_almost_equal(max(res2[0]), 3219.72822546, decimal=7)
+    numpy.testing.assert_almost_equal(min(res2[0]), -1150.76132785, decimal=7)
+    numpy.testing.assert_equal(max(res2[1]), 90448)
+    numpy.testing.assert_equal(res2[2].size, 90450)
 
 def test_get_mtx_b():
     a = 'sample_inputs/sample_mtx.mtx'
