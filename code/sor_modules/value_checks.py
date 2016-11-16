7eac03002690c938becb3e49c0f2555b3d4db1ff
Peter Adam <peter.adam02@gmail.com>
2016-11-16 22:00:00 +0000

    Update value_checks() documentation

--- code/sor_modules/value_checks.py
+++ code/sor_modules/value_checks.py
@@ -1,13 +1,7 @@
 """
 value_checks.py
 
-This module contains 3 functions:
-
-  - zero_diag();
-  - diag_dominant(); and
-  - value_tests().
-
-zero_diag() takes 3 arguments. These are:
+This module contains 1 function value_tests() that takes 4 arguments:
 
   - val - A 1 dimensional numpy array containing the input matrix (A) values in
 CRS format.
@@ -15,48 +9,13 @@ CRS format.
 input matrix (A) entries in val.
   - rowStart - A 1 dimensional numpy array containing the reference indices for
 the beginning of each new row of the the input matrix (A).
+output_filename - A file name to write errors to
 
-zero_diag() aims to identify any 0's on the diagonal of a matrix in CSR
+value_tests() identifies any 0's on the diagonal of a matrix in CSR
 format. It does this by cycling through each row of the matrix, and finding
 the value where row number = column number, ie the diagonal entry. If this
-number is not present (aka zero in a CSR format), the function returns False.
-This is repeated for all rows.
-
-
-diag_dominant() takes 3 arguments. These are:
-
-  - val - A 1 dimensional numpy array containing the input matrix (A) values in
-CRS format.
-  - col - A 1 dimensional numpy array containing the column references for the
-input matrix (A) entries in val.
-  - rowStart - A 1 dimensional numpy array containing the reference indices for
-the beginning of each new row of the the input matrix (A).
-
-diag_dominant() calculates the sum of the absolute values of entries in each
-row, and the sum of the absolute values of entries in each column. It then
-subtracts the absolute value of the diagonal element. If the absolute value
-of the diagonal element is greater than the remainder, that row / column is
-deemed to be strictly row / column diagonally dominant.
-
-If this is true for all rows or columns in a matrix, the matrix is strictly
-diagonally dominant. For conversion of SOR, the input matrix must either be
-row or column diagonally dominant. diag_dominant() calculates both, and if
-either is satisfied, returns True. Otherwise, returns False.
-
-
-value_tests() takes 4 arguments. These are:
-
-  - val - A 1 dimensional numpy array containing the input matrix (A) values in
-CRS format.
-  - col - A 1 dimensional numpy array containing the column references for the
-input matrix (A) entries in val.
-  - rowStart - A 1 dimensional numpy array containing the reference indices for
-the beginning of each new row of the the input matrix (A).
-errors - A list of errors, potentially empty.
-
-value_tests() applies zero_diag() and diag_dominant() to the input matrix,
-and if they fail, appends a given error statement to the errors list. The
-errors list is returned.
+number is not present (aka zero in a CSR format), the alerts the user and
+writes an output file. This is repeated for all rows.
 
 Requirements: numpy, write_output
 
@@ -64,7 +23,7 @@ Requirements: numpy, write_output
 
 from sor_modules import write_output
 
-def value_tests(val, col, rowStart, errors, output_file_name):
+def value_tests(val, col, rowStart, output_file_name):
 
     # Check if the matrix has zeros on the diagonal:
 
@@ -79,4 +38,5 @@ def value_tests(val, col, rowStart, errors, output_file_name):
             # Output False
             write_output.output_text_file(output_file_name, "Zero on diagonal")
             exit("There are zeros on the diagonal")
+
         row += 1
