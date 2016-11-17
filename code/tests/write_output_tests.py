d3c303f2d2be8a169ff5df54153aa390b9d4c784
mcsweena <andy.mcsweeney91@gmail.com>
2016-11-17 19:25:20 +0000

    Updated write_output_tests file

--- code/tests/write_output_tests.py
+++ code/tests/write_output_tests.py
@@ -5,8 +5,3 @@ from sor_modules import write_output
 
 def test_output_test_file():
     pass
-    # a = 'nas_Sor.out'
-    #
-    # res1 = write_output.output_text_file(a, "x Sequence Convergence", 100, 31, 2.22044604925e-16, 1e-10, 1.8884963934515226e-10, 0, [-0.00979991834387, 0.0828909759224, 0.0639036341383, 0.281849734586])
-    #
-    # assert_equal(res1[0], a)
