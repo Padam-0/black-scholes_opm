982986bfefedd13e9325a9454d6dea3bb6253122
Peter Adam <peter.adam02@gmail.com>
2016-11-17 12:27:40 +0000

    Updated optimise_w.py documentation

--- code/sor_modules/optimise_w.py
+++ code/sor_modules/optimise_w.py
@@ -22,14 +22,29 @@ Requirements: random
 import random
 
 def op_w(list, w):
+    # Calculate relative change in error on current iteration
     change = list[-1] / list[-2]
+
+    # Calulate relative change in error on previous iteration
     prev_change = (list[-2] / list[-3])
+
+    # If current change is similar or faster than previous change:
     if change > prev_change - 0.05:
+        # Keep same w
         return w
+    # Otherwise
     else:
+        # Choose a random direction up or down
         m = (random.randint(0,1) * 2 - 1)
-        i = w + 0.05 * m
-        if i > 1 and i < 1.5:
-            return i
+
+        # Add / Subtract 0.05 from w
+        new_w = w + 0.05 * m
+
+        # If new value for w is within 1 and 1.5 inclusive
+        if new_w >= 1 and new_w <= 1.5:
+            # Return new w value
+            return new_w
+        # Otherwise
         else:
+            # Return w +/- 0.05 in the other direction
             return w + 0.05 * (-1 * m)
