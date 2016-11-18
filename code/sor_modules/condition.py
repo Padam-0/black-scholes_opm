47b9dc5bf1a9cdadb85a62e25f29ced46e23cad7
Kieron Ellis <kieron.ellis@ucdconnect.ie>
2016-11-18 18:11:41 +0000

    updated create_matrices program

--- code/sor_modules/condition.py
+++ code/sor_modules/condition.py
@@ -10,12 +10,26 @@ def condition(m, n):
     return relative_condition
 
 def create_dense(row, col, vals, n):
-        m = csr_matrix((vals, (row, col)), shape=(n, n)).todense()
-        return m
-m= np.array([[ 0.01513653,  0.91844431],[ 0.47155416,  0.32966512]])
-#not_diag_dom_evls_less_than_one3.in, condition = 2.69149789298
-m=np.array([[0.07632839, 0.61860076],[0.59402266, 0.51434347]])
-print(condition(m,2))
+    m = csr_matrix((vals, (row, col)), shape=(n, n)).todense()
+    return m
+
+# m= np.array([[ 0.01513653,  0.91844431],[ 0.47155416,  0.32966512]])
+# m=np.array([[0.07632839, 0.61860076],[0.59402266, 0.51434347]])
+
+
+# greater_than_1 = np.array([[ 0.32485338,  0.75049587],[ 0.71390588,  0.22020623]])
+# C_greater_than_1=np.array([[ 7.48983541,  0.        ], [-3.24198766,  0.        ]])
+# greater_than_2 = np.array([[ 0.01124162,  0.90960554],[ 0.7327776 ,  0.10667227]])
+# greater_than_3 = np.array([[ 0.13534856,  0.44150804],[ 0.85794161,  0.09988733]])
+# print(condition(C_greater_than_1,2))
+#
+# print(condition(greater_than_1,2))
+# print(condition(greater_than_2,2))
+# print(condition(greater_than_3,2))
+
+
+
+
 # # example inputs and output
 # row = np.array([0,0,1,2,2,2])
 # col = np.array([0,2,1,0,1,2])
